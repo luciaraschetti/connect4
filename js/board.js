6aa6d9898ac2c1088cca22d6f7ec1097f5a4e15b
lucia raschetti <lucia.sn011@gmail.com>
2020-08-01 05:06:02 -0300

    C4: Add Board class

---
+++ js/board.js
@@ -0,0 +1,47 @@
+class Board {
+    constructor(boardHTML, columnsHTML, board) {
+        this.boardHTML = boardHTML;
+        this.columnsHTML = columnsHTML;
+        this.board = board;
+    }
+
+    resetBoard = function() {
+        for(var i = 0; i < this.board.length; i++) {
+            for(var j = 0; j < this.board[i].length; j++) {
+                this.board[i][j] = null;
+            }
+        }
+    }
+    
+    columnEventHandler = function(evt) {
+        var columnId = evt.target.id.substr(1, 1);
+        for(var i = 0; i < this.board[columnId].length; i++) {
+            if(!this.board[columnId][i]) {
+                this.render();
+                break;
+            }
+        }
+    }
+
+    bindColumnHandlers = function() {
+        for(var i = 0; i < this.columnsHTML.length; i++) {
+            this.columnsHTML[i].onclick = this.columnEventHandler.bind(this);
+        }
+    }
+
+    render = function() {
+        var html = '';
+        for(var i = 0; i < this.board.length; i++) {
+            html += '<div id="c' + i + '" class="column">';
+            for(var j = this.board[i].length - 1; j >= 0; j--) {
+                html += '<div id="s' + i + j + '" class="slot';
+                if(this.board[i][j]) {html += ' ' + this.board[i][j];}
+                html += '"></div>';
+            }
+            html += '</div>';
+        }
+        this.boardHTML.innerHTML = html;
+        this.bindColumnHandlers();
+    }
+    
+}
--- js/game.js
+++ js/game.js
@@ -4,8 +4,9 @@ var p1Name = null;
 var p2Name = null;
 var boardHTML = null;
 var columnsHTML = null;
+var board = null;
 
-var board = [
+var twoPlayerBoard = [
     [null, null, null, null, null, null],
     [null, null, null, null, null, null],
     [null, null, null, null, null, null],
@@ -21,43 +22,12 @@ var getPlayerNames = function() {
     p2Name.innerHTML = savedNames[0].namep2 + ' (P2)';
 }
 
-var columnEventHandler = function(evt) {
-    var columnId = evt.target.id.substr(1, 1);
-    for(var i = 0; i < board[columnId].length; i++) {
-        if(!board[columnId][i]) {
-            renderBoard();
-            break;
-        }
-    }
-}
-
-var bindColumnHandlers = function() {
-    columnsHTML = document.getElementsByClassName('column');
-    for(var i = 0; i < columnsHTML.length; i++) {
-        columnsHTML[i].onclick = columnEventHandler;
-    }
-}
-
-var renderBoard = function() {
-    var html = '';
-    for(var i = 0; i < board.length; i++) {
-        html += '<div id="c' + i + '" class="column">';
-        for(var j = board[i].length - 1; j >= 0; j--) {
-            html += '<div id="s' + i + j + '" class="slot';
-            if(board[i][j]) {html += ' ' + board[i][j];}
-            html += '"></div>';
-        }
-        html += '</div>';
-    }
-    boardHTML.innerHTML = html;
-    bindColumnHandlers();
-}
-
 window.onload = function() {
     p1Name = document.getElementById('p1-name');
     p2Name = document.getElementById('p2-name');
     columnsHTML = document.getElementsByClassName('column');
     boardHTML = document.getElementById('play-area');
     getPlayerNames();
-    renderBoard();
+    board = new Board(boardHTML, columnsHTML, twoPlayerBoard);
+    board.render();
 }
