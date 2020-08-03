8e1e45692a93cde192c729b7bfcf720419358df8
lucia raschetti <lucia.sn011@gmail.com>
2020-08-03 04:59:28 -0300

    C4: Limit saved games list to 5 per section

--- js/loadgame.js
+++ js/loadgame.js
@@ -6,13 +6,26 @@ var p1HTML = null;
 var p2HTML = null;
 var dateHTML = null;
 
+//Displays only the first 5 saved games by sectioning an array made from 
+//
 var loadSavedGamesData = function() {
+    var start = 0;
+    var end = 5;
     savedGames = JSON.parse(localStorage['savedGames']);
+    var listSection = Array.from(gameLI).slice(start, end);
 
     for(var i = 0; i < savedGames.length; i++) {
         p1HTML[i].innerHTML = savedGames[i].p1.name;
         p2HTML[i].innerHTML = savedGames[i].p2.name;
         dateHTML[i].innerHTML =  savedGames[i].date;
+    }
+
+    for(var l = 0; l < savedGames.length; l++) {
+        gameLI[l].className = 'game hidden';
+    }
+
+    for(var j = 0; j < savedGames.length; j++) {
+        listSection[j].className = ' game';
     }
 }
 
@@ -21,7 +34,7 @@ var renderList = function() {
     var html = '';
 
     for(var i = 0; i < storedGames; i++) {
-        html += '<li tabindex="-1" class="game">';
+        html += '<li tabindex="-1" class="game hidden">';
         html += '<div class="match-container">';
         html += '<div class="game-info p1"></div>';
         html += '<p>VS</p>';
